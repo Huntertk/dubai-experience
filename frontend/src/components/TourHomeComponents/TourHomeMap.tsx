f67bd5723cd2ded196ebf5488af6bb3a535c38cc
Huntertk <taufik.khan65@gmail.com>
2024-09-26 16:13:01 +0530

    minor ui error fixed

--- frontend/src/components/TourHomeComponents/TourHomeMap.tsx
+++ frontend/src/components/TourHomeComponents/TourHomeMap.tsx
@@ -3,7 +3,7 @@ const TourHomeMap = ({serviceName, mapUrl}:{serviceName:string; mapUrl:string}) 
   return (
     <div style={{margin:'2rem  auto', width: '100%', display:'flex', alignItems:'center', flexDirection:'column'}}>
 
-      <h1 style={{margin:'1rem'}}>{serviceName}</h1>
+      <h1 style={{margin:'1rem', textAlign:'center'}}>{serviceName}</h1>
       
       <iframe width="360" height="400" src={`${mapUrl}`}></iframe>
     </div>
