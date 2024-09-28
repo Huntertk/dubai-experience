4d0647778886aed1489294dda8aa83def271db5d
Huntertk <taufik.khan65@gmail.com>
2024-09-28 15:16:12 +0530

    code generate script written

--- backend/src/utils/generateRandomCode.ts
+++ backend/src/utils/generateRandomCode.ts
@@ -1,10 +1,21 @@
 import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
 
 const generateRandomCode = async ():Promise<string> => {
     return crypto.randomBytes(8).toString('hex')
 }
 
+const codesArray:string[] = []
 for (let index = 0; index < 20; index++) {
-    generateRandomCode().then(val => console.log(val));
+    generateRandomCode().then(val => {
+        codesArray.push(val)
+    }).then(() => {
+        fs.writeFile(path.join(__dirname,'generated-code.json'),JSON.stringify(codesArray),(err) => {
+            if(err){
+                    console.log(err);   
+                }
+            })
+    });
     
 }
